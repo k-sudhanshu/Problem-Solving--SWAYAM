b7e583126739f86b60012d149626015d81ee8e2d
k-sudhanshu <111634423+k-sudhanshu@users.noreply.github.com>
2024-05-10 01:30:04 +0530

    Create House robber 2.cpp

---
+++ DP_1/House robber 2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        //0 n-2  max 1st case
+        // 1 n-1
+        int n=nums.size();
+        if(n==1)return nums[0];
+        if(n==2)return max(nums[0],nums[1]);
+      
+        int dp[n];
+       dp[0]=nums[0];
+       dp[1]=max(nums[0],nums[1]);
+       for(int i=2;i<n-1;i++){
+           dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+       }
+       int x=dp[n-2];
+       dp[1]=nums[1];
+       dp[2]=max(nums[1],nums[2]);
+       for(int i=3;i<=n-1;i++){
+           dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+       }
+    int ans=max(x,dp[n-1]);
+    return ans;
+
+        
+    }
+};
