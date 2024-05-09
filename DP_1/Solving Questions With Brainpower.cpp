332fcb1a1e962dd5a518341ae4a5142bd1da0ba1
k-sudhanshu <111634423+k-sudhanshu@users.noreply.github.com>
2024-05-10 01:35:34 +0530

    Create Solving Questions With Brainpower.cpp

---
+++ DP_1/Solving Questions With Brainpower.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& v) {
+        int n = v.size();
+        long long ans = LLONG_MIN;
+        vector<long long> dp(n+1, 0);
+        for(int i=n-1; i>=0; i--){
+            long long p = 0;
+            // If it is exceding the n, then we will not include it.
+            if(i + v[i][1] + 1 <= n){
+                p = dp[i + v[i][1] + 1];
+            }
+            dp[i] = max(p + v[i][0], dp[i+1]);
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
