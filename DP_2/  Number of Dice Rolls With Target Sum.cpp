1e9a01a6dcaaaa11cbbcf103dee0fba1ff639320
k-sudhanshu <111634423+k-sudhanshu@users.noreply.github.com>
2024-05-11 00:39:58 +0530

    Update   Number of Dice Rolls With Target Sum.cpp

--- DP_2/  Number of Dice Rolls With Target Sum.cpp
+++ DP_2/  Number of Dice Rolls With Target Sum.cpp
@@ -1,3 +1,54 @@
+/*
+    int helper(int d, int f, int target, vector<vector<int>>& dp, int m) {
+        if (d == 0 && target == 0)
+            return 1;
+        if (d == 0 || target <= 0)
+            return 0;
+        if (dp[d][target] != -1)
+            return dp[d][target];
+
+        int ways = 0;
+        for (int k = 1; k <= f; k++) {
+            if (k <= target)
+                ways = (ways + helper(d - 1, f, target - k, dp, m)) % m;
+        }
+
+        dp[d][target] = ways;
+        return ways;
+    }
+    int numRollsToTarget(int d, int f, int target) {
+        int m = 1000000007;
+        vector<vector<int>> dp(d + 1, vector<int>(target + 1, -1));
+        return helper(d, f, target, dp, m);
+    }
+    
+
+    int helper(int d, int f, int target, vector<vector<int>>& memo, int m) {
+        if (d == 0 && target == 0)
+            return 1;
+        if (d == 0 || target <= 0)
+            return 0;
+        if (memo[d][target] != -1)
+            return memo[d][target];
+
+        int ways = 0;
+        for (int k = 1; k <= f; k++) {
+            if (k <= target)
+                ways = (ways + helper(d - 1, f, target - k, memo, m)) % m;
+        }
+
+        memo[d][target] = ways;
+        return ways;
+    }
+
+ int numRollsToTarget(int d, int f, int target) {
+        int m = 1000000007;
+        vector<vector<int>> memo(d + 1, vector<int>(target + 1, -1));
+        return helper(d, f, target, memo, m);
+    }
+    };
+    
+    */
 class Solution {
 public:
     int numRollsToTarget(int d, int f, int target) {
