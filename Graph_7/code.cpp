f7c7d08242c99457636e9e88102bbbb452954a66
k-sudhanshu <111634423+k-sudhanshu@users.noreply.github.com>
2024-05-11 16:28:31 +0530

    Update code.cpp

--- Graph_7/code.cpp
+++ Graph_7/code.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        int n=nums.size();
-      //  [3,4,5,1,2]
-
-        int s=0;
-        int e=n-1;
-        int ans=INT_MAX;
-        while(s<e){
-            int m=(s+e)/2;
-            if(nums[m]>nums[e]){
-                s=m+1;
-            }
-            else
-                ans=min(ans,m);
-
+    int find(int x,vector<int>& parent){
+        if(parent[x]==x)return x;
+        else
+        return find(parent[x],parent);
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        //basicaly here the idea is every connection between node should be unique, if any node is making cycle this should be removed, 
+        //so in disjoint set we say when parent of 2 nodes is same means it should be removed
+        int n=edges.size();//no.of nodes
+        if(n==0)return {};
+        vector<int>parent(n+1);
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
         }
-       return nums[ans];
+        for(int i=0;i<edges.size();i++){
+            int par1=find(edges[i][0],parent);
+            int par2=find(edges[i][1],parent);
+            if(par1==par2){
+                return edges[i];
+            }else{
+                parent[par1]=par2;
+            }
+        }
+        return {};
+ 
+        
     }
 };
