9406f1e011aac18ff0381b02f1ea2f61f6a6f8d7
k-sudhanshu <111634423+k-sudhanshu@users.noreply.github.com>
2024-05-11 17:50:51 +0530

    Update perfect Square.cpp

--- DP_1/perfect Square.cpp
+++ DP_1/perfect Square.cpp
@@ -16,3 +16,15 @@ public:
     return dp[n];
     }
 };
+/* int helper(int n,vector<int>& dp) {
+        if (n <= 0) return 0;
+        if (dp[n] != INT_MAX) return dp[n];
+        
+        int sqrt_n = sqrt(n);
+        for (int i = 1; i <= sqrt_n; ++i) {
+            int square = i * i;
+            dp[n] = std::min(dp[n], helper(n - square, dp) + 1);
+        }
+        
+        return dp[n];
+    }*/
